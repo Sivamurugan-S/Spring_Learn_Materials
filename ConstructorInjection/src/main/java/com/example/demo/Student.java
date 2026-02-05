20632b4f577353ed7dfdf24b25bdf8aea8b231ac
sivamurugan <sivamurugan@localhost>
2026-02-05 10:05:27 +0530

    Initial project commit

---
+++ ConstructorInjection/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	Laptop ref;
+	
+	void study() {
+		System.out.println("Student is studying.");
+	}
+	
+	void useLaptop() {
+		ref.work();
+	}
+	
+	@Autowired
+	public Student(Laptop ref) {
+		super();
+		this.ref = ref;
+	}
+}
