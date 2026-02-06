eb775960f5659a314e389efcb633001a9591ef65
Sivamurugan-S <137901619+Sivamurugan-S@users.noreply.github.com>
2026-02-06 11:49:53 +0530

    Update Student.java
    Added some Constructor injection questions and answers

--- ConstructorInjection/src/main/java/com/example/demo/Student.java
+++ ConstructorInjection/src/main/java/com/example/demo/Student.java
@@ -15,9 +15,16 @@ public class Student {
 		ref.work();
 	}
 	
-	@Autowired
-	public Student(Laptop ref) {
+	@Autowired // Spring handles using @Autowired annotation
+	public Student(Laptop ref) { 
 		super();
 		this.ref = ref;
 	}
 }
+
+// What is Constructor Injection?
+// Contsructor Injection is the process of providing the required object to the class through its constructor.
+// What is @Autowired?
+// @Autowired annotation tells Spring to perform automatically injection.
+// Why Constructor injection used more than setter injection?
+// Constructor injection is preffered for mandatory injection. Object cannot be created without required inputs.
